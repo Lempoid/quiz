4feaf8fa2d4c2b327e80bde478f34c73fe586433
Alex <alexbreger123@gmail.com>
2024-03-18 02:40:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for item in question_data:
+    question_bank.append(Question(item["text"], item["answer"]))
+
+brain = QuizBrain(question_bank)
+brain.next_question()
+
+while brain.still_has_questions():
+    brain.next_question()
